43c6479c5bd4316b32089daee92f9e771c881480
mensur056 <mansur.sarxanov@gmail.com>
2022-03-19 21:02:47 +0100

    test

--- lib/screens/home_page/body.dart
+++ lib/screens/home_page/body.dart
@@ -1,14 +1,12 @@
-import 'package:data_camp/Widgets/on_hover_button.dart';
 import 'package:flutter/material.dart';
-import '../Screen_Part/datacamp_skills.dart';
+import '../Screen_Part/DataCamp_skills.dart';
 import '../Screen_Part/first_part_bar.dart';
 import '../part_decoration/first_part_decoration.dart';
 import '../Screen_Part/fourth_part_bar.dart';
-import '../Screen_Part/proqram_language_icons.dart';
+import '../Screen_Part/program_language_icons.dart';
 import '../part_decoration/second_part_decoration.dart';
 import '../part_decoration/third_part_decoration.dart';
-import '../Screen_Part/what_is_datacamp.dart';
-import '../../constants.dart';
+import '../Screen_Part/what_is_DataCamp.dart';
 
 class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
