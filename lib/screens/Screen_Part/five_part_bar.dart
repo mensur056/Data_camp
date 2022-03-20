5117a6a50f0bafd17ecdcdae8781a876449ccdce
mensur056 <mansur.sarxanov@gmail.com>
2022-03-20 14:12:14 +0100

    test

---
+++ lib/screens/Screen_Part/five_part_bar.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+class FivePartBar extends StatelessWidget {
+  const FivePartBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'Hands-on',
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 40,
+                  fontFamily: 'CarterOne'),
+            ),
+            Text(
+              ' learning experience',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontFamily: 'CarterOne'),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/123.png',
+            ),
+            const SizedBox(
+              width: 80,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'No installation required ',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                      fontFamily: 'VarelaRound'),
+                ),
+                Text(
+                  '— run code from your browser',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      fontFamily: 'VarelaRound'),
+                )
+              ],
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 100,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/Screenshot.png',
+            ),
+            const SizedBox(
+              width: 100,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Learn from the                   ',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontFamily: 'VarelaRound',
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'best instructors',
+                  style: TextStyle(
+                      color: Colors.deepPurpleAccent,
+                      fontSize: 25,
+                      fontFamily: 'VarelaRound',
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 100,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/123.png',
+            ),
+            const SizedBox(
+              width: 70,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'No installation required —',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 35,
+                      fontFamily: 'VarelaRound'),
+                ),
+                Text(
+                  ' run code from your browser',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontFamily: 'VarelaRound'),
+                )
+              ],
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 100,
+        )
+      ],
+    );
+  }
+}
--- lib/screens/home_page/body.dart
+++ lib/screens/home_page/body.dart
@@ -3,6 +3,7 @@ import 'package:data_camp/screens/part_decoration/fourth_part_deroration.dart';
 import 'package:flutter/material.dart';
 import '../Screen_Part/DataCamp_skills.dart';
 import '../Screen_Part/first_part_bar.dart';
+import '../Screen_Part/five_part_bar.dart';
 import '../part_decoration/first_part_decoration.dart';
 import '../Screen_Part/fourth_part_bar.dart';
 import '../Screen_Part/program_language_icons.dart';
@@ -27,217 +28,28 @@ class _BodyState extends State<Body> {
             height: 50,
           ),
           Column(
-            children: [
-              const FirstPartBar(),
-              const SizedBox(
+            children: const [
+              FirstPartBar(),
+              SizedBox(
                 height: 80,
               ),
-              const ProqramLanguageIcons(),
-              const SizedBox(
+              ProqramLanguageIcons(),
+              SizedBox(
                 height: 60,
               ),
-              const FirstPartDecoration(),
-              const DataCampSkills(),
-              const SecondPartDecoration(),
-              const WhatIsDataCamp(),
-              const ThirdPartDecoration(),
-              const FourthPartBar(),
-              const FourthPartDecoration(),
-              const SizedBox(
+              FirstPartDecoration(),
+              DataCampSkills(),
+              SecondPartDecoration(),
+              WhatIsDataCamp(),
+              ThirdPartDecoration(),
+              FourthPartBar(),
+              FourthPartDecoration(),
+              SizedBox(
                 height: 50,
               ),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        'Hands-on',
-                        style: TextStyle(
-                            color: Colors.green,
-                            fontSize: 40,
-                            fontFamily: 'CarterOne'),
-                      ),
-                      Text(
-                        ' learning experience',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40,
-                            fontFamily: 'CarterOne'),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'images/123.png',
-                      ),
-                      const SizedBox(
-                        width: 80,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            'No installation required ',
-                            style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 35,
-                                fontFamily: 'VarelaRound'),
-                          ),
-                          Text(
-                            '— run code from your browser',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                                fontFamily: 'VarelaRound'),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 100,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'images/Screenshot.png',
-                      ),
-                      const SizedBox(
-                        width: 100,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            'Learn from the                   ',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 35,
-                                fontFamily: 'VarelaRound',
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'best instructors',
-                            style: TextStyle(
-                                color: Colors.deepPurpleAccent,
-                                fontSize: 25,
-                                fontFamily: 'VarelaRound',
-                                fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 100,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'images/123.png',
-                      ),
-                      const SizedBox(
-                        width: 70,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            'No installation required —',
-                            style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 35,
-                                fontFamily: 'VarelaRound'),
-                          ),
-                          Text(
-                            ' run code from your browser',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 25,
-                                fontFamily: 'VarelaRound'),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 100,
-                  )
-                ],
-              ),
-              const FirstPartDecoration(),
-              Container(
-                width: double.infinity,
-                height: 1200,
-                color: const Color(0xFFf7f3eb),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      color: kPrColor,
-                      child: Column(
-                        children: const [
-                          Text(
-                            'Learning paths designed by experts',
-                            style: TextStyle(color: Colors.white, fontSize: 30),
-                          ),
-                          Text('Learn a new technology                    ',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 30)),
-                        ],
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          color: Colors.white,
-                          height: 100,
-                          width: 180,
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Container(
-                          color: Colors.white,
-                          height: 100,
-                          width: 180,
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Container(
-                          color: Colors.white,
-                          height: 100,
-                          width: 180,
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Container(
-                          color: Colors.white,
-                          height: 100,
-                          width: 180,
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              )
+              FivePartBar(),
+              FirstPartDecoration(),
+
             ],
           ),
         ],
@@ -245,3 +57,4 @@ class _BodyState extends State<Body> {
     );
   }
 }
+
