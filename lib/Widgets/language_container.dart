2b0759e9c073c46024c7a06d501bb2bfe6475114
mensur056 <mansur.sarxanov@gmail.com>
2022-03-23 20:49:15 +0100

    DataCamp

--- lib/Widgets/language_container.dart
+++ lib/Widgets/language_container.dart
@@ -1,6 +1,8 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 
-class LanguageBar extends StatelessWidget {
+class LanguageBar extends StatefulWidget {
   const LanguageBar({
     Key? key,
     required this.barHeight,
@@ -11,17 +13,31 @@ class LanguageBar extends StatelessWidget {
   final double barWidth;
 
   @override
+  State<LanguageBar> createState() => _LanguageBarState();
+}
+
+class _LanguageBarState extends State<LanguageBar> {
+  double heightSide = 100;
+  double widthSide = 240;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: barHeight,
-      width: barWidth,
-      child: const Center(
-        child: Text(
-          'Python',
-          style: TextStyle(color: Colors.blue),
+    return AnimatedContainer(
+      height: heightSide,
+      width: widthSide,
+      duration: const Duration(seconds: 1),
+      curve: Curves.easeIn,
+      child: Material(
+        color: Colors.blue,
+        child: InkWell(
+          onTap: () {
+            setState(() {
+              heightSide == 100 ? heightSide = 50 : heightSide = 100;
+            });
+          },
+          child: const Center(child: Text('python')),
         ),
       ),
-      color: Colors.white,
     );
   }
 }
